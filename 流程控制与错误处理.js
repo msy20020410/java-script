c61eb24dc5483b46351b6ddca6c943003c4b3d7a
msy20020410 <dj8817117@gmail.com>
2025-09-25 15:31:38 +0800

    02_js流程控制与错误处理

---
+++ 流程控制与错误处理.js
@@ -0,0 +1,30 @@
+/**
+ * 教程地址：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Guide/Control_flow_and_error_handling
+ */
+
+// 注意点
+// const b = new Boolean(false);
+// if (b) {
+//     console.log("对象是真的！");
+// }
+// if (b === true) {
+//     console.log("对象值是假的！");
+// }
+
+// switch
+// const alphabet = 'a';
+// switch (alphabet) {
+//     case 'a':
+//         console.log('a');
+//         break;
+//     case 'b':
+//         console.log('b');
+//         break;
+//     default:
+//         console.log('没有匹配项！');
+// }
+
+// 异常
+// throw new Error('这是一个错误！');
+
+
