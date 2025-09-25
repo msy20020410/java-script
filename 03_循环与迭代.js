0f60f0d8d6f4dc7d421b65f6bcabcc1fa2241ab6
msy20020410 <dj8817117@gmail.com>
2025-09-25 16:07:09 +0800

    03_js循环与迭代

---
+++ 03_循环与迭代.js
@@ -0,0 +1,46 @@
+/**
+ * 教程地址：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Guide/Loops_and_iteration
+ */
+
+// var num = 0;
+// outLoop:for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         if (j === 5 && i === 5) {
+//             break outLoop;
+//         }
+//         num++;
+//     }
+// }
+// console.log(num);
+//
+// var num = 0;
+// outLoop:for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         if (j === 5 && i === 5) {
+//             continue outLoop;
+//         }
+//         num++;
+//     }
+// }
+// console.log(num);
+
+// for in 的使用
+// var person = {
+//     name: '张三',
+//     age: 18
+// }
+// for (let key in person) {
+//     console.log(`${key}=${person[key]}`);
+// }
+
+// var names = ['张三', '李四', '王五'];
+// // for in 遍历的是数组的索引
+// for (const index in names) {
+//     console.log(index);
+// }
+// // for of 遍历的是数组的值
+// for (let name of names) {
+//     console.log(name);
+// }
+
+
