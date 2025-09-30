799a07b1e1d9cf41a15de012ababccf0cc180661
msy20020410 <dj8817117@gmail.com>
2025-09-30 11:45:13 +0800

    06_数字和字符串

---
+++ 06_数字和字符串.js
@@ -0,0 +1,16 @@
+/**
+ * 教程地址：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Guide/Numbers_and_strings
+ */
+
+// Number
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.POSITIVE_INFINITY);
+// console.log(typeof Number.NEGATIVE_INFINITY);
+
+// Date
+console.log(Date.now());
+console.log(new Date());
+console.log(new Date().toLocaleString());
