3baf0765865a9e0e77e7e73d8d1c9ed514435d61
msy20020410 <dj8817117@gmail.com>
2025-09-30 14:39:08 +0800

    07_索引集合类

---
+++ 07_索引和集合.js
@@ -0,0 +1,38 @@
+/**
+ * 教程地址：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Guide/Indexed_collections
+ */
+
+
+
+// 数组
+// const myArray = ["Wind", "Rain", "Fire"];
+// myArray.forEach(value => console.log(value));
+
+// let myArray2 = [1, 2, 3, 4, 5];
+// console.log(myArray2.slice(1, 4));  // 提取数组片段，并返回新的数组
+
+// 将数组展平
+// let arr = [1, 2, [3, 4, 5], 6];
+// console.log(arr.flat());
+
+const arr = [1, 2, 3, 4, 5];
+// const a2 = arr.map(item => {
+//     if (item % 2 === 0) {
+//         return item * 2;
+//     } else {
+//         return item * 3;
+//     }
+// })
+// console.log(a2);
+
+// const a3=arr.filter(item => item % 2 === 0);
+// console.log(a3);
+
+// const total = arr.reduce((sum, item) => sum + item, 100);
+// console.log(total);
+
+arr.aAtr = '嘿嘿';
+console.log(arr.aAtr);
+
+
+
